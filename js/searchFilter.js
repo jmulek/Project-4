14c8402b82101c8ce601f7cf2a68053df2296f07
Jimmy Mulek <jmulek218@gmail.com>
2016-03-19 17:53:51 -0500

    added style to hover

--- js/searchFilter.js
+++ js/searchFilter.js
@@ -1,32 +1,31 @@
-(function() {                                     // Lives in an IIFE
-  var $imgs = $('#lightgallery img');             // Get the images
-  var $search = $('#filter-search');              // Get the input element
-  var cache = [];                                 // Create an array called cache
-
-  $imgs.each(function() {                         // For each image
-    cache.push({                                  // Add an object to the cache array
-      element: this,                              // This image
-      text: this.alt.trim().toLowerCase()         // Its alt text (lowercase trimmed)
+(function() {                                   
+  var $imgs = $('#lightgallery img');            
+  var $search = $('#filter-search');              
+  var cache = [];                                
+  $imgs.each(function() {                         
+    cache.push({                                  
+      element: this,                              
+      text: this.alt.trim().toLowerCase()         
     });
   });
 
-  function filter() {                             // Declare filter() function
-    var query = this.value.trim().toLowerCase();  // Get the query
-    cache.forEach(function(img) {                 // For each entry in cache pass image
-      var index = 0;                              // Set index to 0
+  function filter() {                            
+    var query = this.value.trim().toLowerCase();  
+    cache.forEach(function(img) {                 
+      var index = 0;                             
 
-      if (query) {                                // If there is some query text
-        index = img.text.indexOf(query);          // Find if query text is in there
+      if (query) {                                
+        index = img.text.indexOf(query);        
       }
 
       img.element.style.display = index === -1 ? 'none' : '';  // Show / hide
     });
   }
 
-  if ('oninput' in $search[0]) {                   // If browser supports input event
-    $search.on('input', filter);                   // Use input event to call filter()
-  } else {                                         // Otherwise
-    $search.on('keyup', filter);                   // Use keyup event to call filter()
+  if ('oninput' in $search[0]) {                   
+    $search.on('input', filter);                 
+  } else {                                       
+    $search.on('keyup', filter);                 
   }
 
 }());
